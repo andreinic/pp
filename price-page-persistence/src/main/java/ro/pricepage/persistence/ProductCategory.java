30b1873607253552401ce61beca37fd386619d83
Andrei Nicusan <andreinicusan@yahoo.com>
2012-11-06 23:43:35 +0200

    One more entity

---
+++ price-page-persistence/src/main/java/ro/pricepage/persistence/ProductCategory.java
@@ -0,0 +1,55 @@
+package ro.pricepage.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product_categories")
+public class ProductCategory extends BaseEntity {
+	private static final long serialVersionUID = -3557158368102788220L;
+	private Integer id;
+	private ProductCategory parent;
+	private String name;
+	private String description;
+	
+	@Id
+	@Column(name="id", nullable=false, unique=true)
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name="fk_parent")
+	public ProductCategory getParent() {
+		return parent;
+	}
+	public void setParent(ProductCategory parent) {
+		this.parent = parent;
+	}
+	
+	@Column(name="name", length=100)
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name="description", columnDefinition="text")
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
