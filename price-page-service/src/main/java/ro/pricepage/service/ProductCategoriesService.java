a248aa2cead0f4e241a8127e988315f44b72f7a8
Radu Toev <radutoev@gmail.com>
2012-12-10 22:22:32 +0200

    whatevs
    ba am o problema cu bindingu ala. nu imi dau seama momentan de unde se
    trage

--- price-page-service/src/main/java/ro/pricepage/service/ProductCategoriesService.java
+++ price-page-service/src/main/java/ro/pricepage/service/ProductCategoriesService.java
@@ -2,15 +2,19 @@ package ro.pricepage.service;
 
 import ro.pricepage.persistence.entities.ProductCategory;
 
+import javax.ejb.Stateless;
+import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+import static ro.pricepage.persistence.entities.ProductCategory.*;
+
 /**
- * User: radutoev
- * Date: 30.11.2012
- * Time: 22:46
+ * @author  radutoev
  */
+@Named(value = "productCategoriesService")
+@Stateless
 public class ProductCategoriesService extends BaseService
 {
     private static final long serialVersionUID = 1L;
@@ -18,7 +22,15 @@ public class ProductCategoriesService extends BaseService
     @PersistenceContext
     private EntityManager em;
 
-    public List<ProductCategory> listProductCategories(){
-        return em.createNamedQuery(ProductCategory.GET_CATEGORIES).getResultList();
+    public ProductCategory add(String name){
+        assert name != null && !"".equals(name) : "unsupported parameter value provided";
+        ProductCategory pc = new ProductCategory();
+        pc.setName(name);
+        em.persist(pc);
+        return pc;
+    }
+
+    public List<ProductCategory> list(){
+        return (List<ProductCategory>) em.createNamedQuery(GET_CATEGORIES).getResultList();
     }
 }
--- price-page-web/src/main/java/ro/pricepage/view/CategoriesView.java
+++ price-page-web/src/main/java/ro/pricepage/view/CategoriesView.java
@@ -3,65 +3,89 @@ package ro.pricepage.view;
 import com.ocpsoft.pretty.faces.annotation.URLMapping;
 import org.primefaces.model.DefaultTreeNode;
 import org.primefaces.model.TreeNode;
-import ro.pricepage.persistence.entities.ProductCategory;
+import ro.pricepage.service.ProductCategoriesService;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
- * User: toev
- * Date: 11/1/12
- * Time: 10:24 PM
- * To change this template use File | Settings | File Templates.
+ * @author radutoev
  */
 @ManagedBean(name = "categoriesView")
+@ViewScoped
 @URLMapping(id = "categoriesView", pattern = "/admin/categorii", viewId = "/WEB-INF/view/admin/categories.jsf")
 public class CategoriesView implements Serializable
 {
     private static final long serialVersionUID = 1L;
 
-    private String dummyTxt;
+    @EJB
+    private ProductCategoriesService categoriesService;
+
     private TreeNode root;
+
+    private String prodCatName;
 
     @PostConstruct
     public void init(){
+        System.out.println("init categoriesView");
         root = new DefaultTreeNode("root", null);
-
-        TreeNode cat1 = new DefaultTreeNode(new CategoryDoc("Cat1"), root);
-        TreeNode cat2 = new DefaultTreeNode(new CategoryDoc("Cat2"), root);
-
-        TreeNode cat11 = new DefaultTreeNode("document", new CategoryDoc("Cat11"), cat1);
-        TreeNode cat12 = new DefaultTreeNode("document", new CategoryDoc("Cat12"), cat1);
-        TreeNode cat13 = new DefaultTreeNode("document", new CategoryDoc("Cat12"), cat1);
     }
 
-    public List<String> dummyComplete(String query){
-        List<String> res = new ArrayList<>();
-        for(int i = 0 ; i < 10 ; ++i){
-            res.add(query+i);
-        }
-        return res;
+    // Actions ---------------------------------------------------------------------------------------------
+    public void onSave(){
+        System.out.println(prodCatName);
+//        new DefaultTreeNode(categoriesService.add(prodCatName), root);
     }
 
-    public String getDummyTxt(){ return dummyTxt; }
-    public void setDummyTxt(String val){ dummyTxt = val; }
 
+    // Getters and setters ---------------------------------------------------------------------------------
     public TreeNode getRoot(){return root;}
 
-    public class CategoryDoc
-    {
-        private String name;
+    public String getProdCatName() { return prodCatName; }
+    public void setProdCatName(String value) { prodCatName = value; }
 
-        public CategoryDoc(String name)
-        {
-            this.name = name;
-        }
 
-        public String getName(){return name;}
-        public void setName(String name){this.name = name;}
-    }
+//    private String dummyTxt;
+//    private TreeNode root;
+//
+//    @PostConstruct
+//    public void init(){
+//        root = new DefaultTreeNode("root", null);
+//
+//        TreeNode cat1 = new DefaultTreeNode(new CategoryDoc("Cat1"), root);
+//        TreeNode cat2 = new DefaultTreeNode(new CategoryDoc("Cat2"), root);
+//
+//        TreeNode cat11 = new DefaultTreeNode("document", new CategoryDoc("Cat11"), cat1);
+//        TreeNode cat12 = new DefaultTreeNode("document", new CategoryDoc("Cat12"), cat1);
+//        TreeNode cat13 = new DefaultTreeNode("document", new CategoryDoc("Cat12"), cat1);
+//    }
+//
+//    public List<String> dummyComplete(String query){
+//        List<String> res = new ArrayList<>();
+//        for(int i = 0 ; i < 10 ; ++i){
+//            res.add(query+i);
+//        }
+//        return res;
+//    }
+//
+//    public String getDummyTxt(){ return dummyTxt; }
+//    public void setDummyTxt(String val){ dummyTxt = val; }
+//
+//    public TreeNode getRoot(){return root;}
+//
+//    public class CategoryDoc
+//    {
+//        private String name;
+//
+//        public CategoryDoc(String name)
+//        {
+//            this.name = name;
+//        }
+//
+//        public String getName(){return name;}
+//        public void setName(String name){this.name = name;}
+//    }
 }
