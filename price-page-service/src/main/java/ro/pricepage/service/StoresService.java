54abd51ce59a896568f688e247b820cf3b4d31ed
unknown <ro1v02n2@RO2CV11C.ww300.siemens.net>
2013-01-10 18:54:43 +0200

    merge solved

--- price-page-service/src/main/java/ro/pricepage/service/StoresService.java
+++ price-page-service/src/main/java/ro/pricepage/service/StoresService.java
@@ -1,104 +1,3 @@
-<<<<<<< HEAD
-package ro.pricepage.service;
-
-import java.util.List;
-
-import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
-import javax.inject.Named;
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
-import ro.pricepage.persistence.entities.Store;
-import ro.pricepage.persistence.entities.StoreChain;
-import ro.pricepage.persistence.entities.StoreType;
-
-@Named("storesService")
-@Stateless
-public class StoresService extends BaseService {
-	private static final long serialVersionUID = -2191833772375756341L;
-
-	@PersistenceContext
-	private EntityManager em;
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-	public Store saveStore(Store store){
-		return em.merge(store);
-	}
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public List<String> findAllStoreChainNames() {
-		TypedQuery<String> q = em.createNamedQuery(StoreChain.Q_FIND_ALL_NAMES,
-				String.class);
-		return q.getResultList();
-	}
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public List<String> findAllStoreTypeNames() {
-		TypedQuery<String> q = em.createNamedQuery(StoreType.Q_FIND_ALL_NAMES,
-				String.class);
-		return q.getResultList();
-	}
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public StoreChain findSingleStoreChainByName(String name) {
-		TypedQuery<StoreChain> q = em.createNamedQuery(StoreChain.Q_FIND_BY_NAME, StoreChain.class)
-				.setParameter("name", name)
-				.setMaxResults(1);
-		try{
-			return q.getSingleResult();
-		}
-		catch(NoResultException e){
-			return null;
-		}
-	}
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public StoreType findSingleStoreTypeByName(String name) {
-		TypedQuery<StoreType> q = em.createNamedQuery(StoreType.Q_FIND_BY_NAME, StoreType.class)
-				.setParameter("name", name)
-				.setMaxResults(1);
-		try{
-			return q.getSingleResult();
-		}
-		catch(NoResultException e){
-			return null;
-		}
-	}
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-	public StoreChain saveChainWithName(String name){
-		StoreChain chain = new StoreChain();
-		chain.setName(name);
-		return em.merge(chain);
-	}
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-	public StoreType saveTypeWithName(String name){
-		StoreType type = new StoreType();
-		type.setName(name);
-		return em.merge(type);
-	}
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public List<String> findAllChainNamesInCity(String city){
-		TypedQuery<String> q = em.createNamedQuery(StoreChain.Q_FIND_ALL_NAMES_IN_CITY, String.class)
-				.setParameter("city", city);
-		return q.getResultList();
-	}
-
-	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public List<Store> findAllStoresByChainAndCity(String chain, String city){
-		TypedQuery<Store> q = em.createNamedQuery(Store.Q_FIND_ALL_BY_CHAIN_AND_CITY, Store.class)
-				.setParameter("chain", chain)
-				.setParameter("city", city);
-		return q.getResultList();
-	}
-}
-=======
 package ro.pricepage.service;
 
 import java.util.List;
@@ -185,5 +84,19 @@ public class StoresService extends BaseService {
 		type.setName(name);
 		return em.merge(type);
 	}
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public List<String> findAllChainNamesInCity(String city){
+		TypedQuery<String> q = em.createNamedQuery(StoreChain.Q_FIND_ALL_NAMES_IN_CITY, String.class)
+				.setParameter("city", city);
+		return q.getResultList();
+	}
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public List<Store> findAllStoresByChainAndCity(String chain, String city){
+		TypedQuery<Store> q = em.createNamedQuery(Store.Q_FIND_ALL_BY_CHAIN_AND_CITY, Store.class)
+				.setParameter("chain", chain)
+				.setParameter("city", city);
+		return q.getResultList();
+	}
 }
->>>>>>> 0729bced4aa0caa1f7f063e0c0e768014660bd0f
