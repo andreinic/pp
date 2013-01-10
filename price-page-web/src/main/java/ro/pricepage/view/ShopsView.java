06b0fe08f62308b12647b723d9f7cfc5ee2e4568
unknown <ro1v02n2@RO2CV11C.ww300.siemens.net>
2013-01-10 17:28:00 +0200

    bla

--- price-page-web/src/main/java/ro/pricepage/view/ShopsView.java
+++ price-page-web/src/main/java/ro/pricepage/view/ShopsView.java
@@ -103,6 +103,19 @@ public class ShopsView implements Serializable
 		Store store = new Store(storeName, chain, type, locality, address, zip, longitude, latitude, url);
 		storesService.saveStore(store);
 	}
+	
+	public Object onTreePick(){
+		getSelectedStore();
+		return null;
+	}
+	
+	public void onUpdate(){
+		
+	}
+	
+	public void onCancelEdit(){
+		
+	}
 
 	public void showOnMap(ActionEvent event){
 		map.getMarkers().clear();
@@ -337,6 +350,37 @@ public class ShopsView implements Serializable
 
 		public void setStore(Store store) {
 			this.store = store;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			result = prime * result + ((store == null) ? 0 : store.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ShopDoc other = (ShopDoc) obj;
+			if (name == null) {
+				if (other.name != null)
+					return false;
+			} else if (!name.equals(other.name))
+				return false;
+			if (store == null) {
+				if (other.store != null)
+					return false;
+			} else if (!store.getId().equals(other.store.getId()))
+				return false;
+			return true;
 		}
 	}
 }
