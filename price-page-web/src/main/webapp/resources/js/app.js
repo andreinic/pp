154342e5d2299598f4724e7591b987b9fb2ab8f7
Radu Toev <radutoev@gmail.com>
2013-02-24 21:17:40 +0200

    minor style changes

--- price-page-web/src/main/webapp/resources/js/app.js
+++ price-page-web/src/main/webapp/resources/js/app.js
@@ -7,13 +7,69 @@ angular.module("price-page", ['ngResource'])
 	            	function openDialog() {
 	              		var element = angular.element('#authModal');
 	              		var ctrl = element.controller();
-	              		ctrl.setModel(arguments[0].currentTarget.name == 'login');
 	              		element.modal('show');
 	            	}
 	            	element.bind('click', openDialog);
 	       		}
 	        }
        		return openDialog;})
+       .directive('simpleCaptcha', function() {
+           return {
+               restrict: 'E',
+               scope: { valid: '=' },
+               template: '<input ng-model="a.value" ng-show="a.input" style="width:2em; text-align: center;"><span ng-hide="a.input">{{a.value}}</span>&nbsp;{{operation}}&nbsp;<input ng-model="b.value" ng-show="b.input" style="width:2em; text-align: center;"><span ng-hide="b.input">{{b.value}}</span>&nbsp;=&nbsp;{{result}}',
+               controller: function($scope) {
+
+                   var show = Math.random() > 0.5;
+
+                   var value = function(max){
+                       return Math.floor(max * Math.random());
+                   };
+
+                   var int = function(str){
+                       return parseInt(str, 10);
+                   };
+
+                   $scope.a = {
+                       value: show? undefined : 1 + value(4),
+                       input: show
+                   };
+                   $scope.b = {
+                       value: !show? undefined : 1 + value(4),
+                       input: !show
+                   };
+                   $scope.operation = '+';
+
+                   $scope.result = 5 + value(5);
+
+                   var a = $scope.a;
+                   var b = $scope.b;
+                   var result = $scope.result;
+
+                   var checkValidity = function(){
+                       if (a.value && b.value) {
+                           var calc = int(a.value) + int(b.value);
+                           $scope.valid = calc == result;
+                       } else {
+                           $scope.valid = false;
+                       }
+                       $scope.$apply(); // needed to solve 2 cycle delay problem;
+                   };
+
+
+                   $scope.$watch('a.value', function(){
+                       checkValidity();
+                   });
+
+                   $scope.$watch('b.value', function(){
+                       checkValidity();
+                   });
+
+
+
+               }
+           };
+       })
        .config(function($routeProvider){
             $routeProvider.when("/", {templateUrl : 'partials/first.html'})
                           .when("/despre-noi", {templateUrl : 'partials/despre-noi.html'})
@@ -26,7 +82,7 @@ angular.module("price-page", ['ngResource'])
        })
        .run(function($rootScope){
             var mapOptions = {
-                zoom : 8,
+                zoom : 12,
                 center : new google.maps.LatLng(45.661327, 25.610161),
                 mapTypeId: google.maps.MapTypeId.ROADMAP,
                 panControl : false,
@@ -37,7 +93,7 @@ angular.module("price-page", ['ngResource'])
                 navigator.geolocation.getCurrentPosition(function (position) {
                     var userCoordinates = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
                     mapOptions = {
-                        zoom : 8,
+                        zoom : 12,
                         center : userCoordinates,
                         mapTypeId: google.maps.MapTypeId.ROADMAP,
                         panControl : false,
