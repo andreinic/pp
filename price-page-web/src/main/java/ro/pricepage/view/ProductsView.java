5be1815c433b18430e5dcfd878f2d8e552bcf822
andreinic <andreinicusan@yahoo.com>
2013-03-09 15:59:14 +0200

    layout for products admin page.

--- price-page-web/src/main/java/ro/pricepage/view/ProductsView.java
+++ price-page-web/src/main/java/ro/pricepage/view/ProductsView.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.event.ActionEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.jboss.seam.faces.context.RenderScoped;
 
 import ro.pricepage.model.ProductsDataModel;
+import ro.pricepage.persistence.entities.Producer;
 import ro.pricepage.persistence.entities.Product;
 import ro.pricepage.persistence.entities.ProductCategory;
 import ro.pricepage.service.ProductCategoriesService;
@@ -33,20 +35,30 @@ public class ProductsView implements Serializable {
 
 	@Inject
 	private ProductCategoriesService categoriesService;
+	
+	private String newProductName;
+	private ProductCategory newProductCategory;
+	private Producer newProductProducer;
+	private String newProductQuantity;
+	private String newProductMeasureUnit; 
+	private String newProductDescription;	
+
+	private String editedProductName;
+	private ProductCategory editedProductCategory;
+	private Producer editedProductProducer;
+	private String editedProductQuantity;
+	private String editedProductMeasureUnit; 
+	private String editedProductDescription;
 
 	@PostConstruct
 	public void init() {
-		selectedProduct = new Product();
 		categories = categoriesService.list();
 	}
 
 	// Actions
 	// ---------------------------------------------------------------------------------------------
-	public void onSave() {
-		if (selectedProduct != null){
-			productsModel.addProduct(selectedProduct);
-		}
-		selectedProduct = new Product();
+	public void onSave(ActionEvent e) {
+		
 	}
 
 	// Getters and setters
@@ -70,5 +82,101 @@ public class ProductsView implements Serializable {
 
 	public ProductsDataModel getProductsModel() {
 		return productsModel;
+	}
+
+	public String getNewProductName() {
+		return newProductName;
+	}
+
+	public void setNewProductName(String newProductName) {
+		this.newProductName = newProductName;
+	}
+
+	public ProductCategory getNewProductCategory() {
+		return newProductCategory;
+	}
+
+	public void setNewProductCategory(ProductCategory newProductCategory) {
+		this.newProductCategory = newProductCategory;
+	}
+
+	public Producer getNewProductProducer() {
+		return newProductProducer;
+	}
+
+	public void setNewProductProducer(Producer newProductProducer) {
+		this.newProductProducer = newProductProducer;
+	}
+
+	public String getNewProductQuantity() {
+		return newProductQuantity;
+	}
+
+	public void setNewProductQuantity(String newProductQuantity) {
+		this.newProductQuantity = newProductQuantity;
+	}
+
+	public String getNewProductMeasureUnit() {
+		return newProductMeasureUnit;
+	}
+
+	public void setNewProductMeasureUnit(String newProductMeasureUnit) {
+		this.newProductMeasureUnit = newProductMeasureUnit;
+	}
+
+	public String getNewProductDescription() {
+		return newProductDescription;
+	}
+
+	public void setNewProductDescription(String newProductDescription) {
+		this.newProductDescription = newProductDescription;
+	}
+
+	public String getEditedProductName() {
+		return editedProductName;
+	}
+
+	public void setEditedProductName(String editedProductName) {
+		this.editedProductName = editedProductName;
+	}
+
+	public ProductCategory getEditedProductCategory() {
+		return editedProductCategory;
+	}
+
+	public void setEditedProductCategory(ProductCategory editedProductCategory) {
+		this.editedProductCategory = editedProductCategory;
+	}
+
+	public Producer getEditedProductProducer() {
+		return editedProductProducer;
+	}
+
+	public void setEditedProductProducer(Producer editedProductProducer) {
+		this.editedProductProducer = editedProductProducer;
+	}
+
+	public String getEditedProductQuantity() {
+		return editedProductQuantity;
+	}
+
+	public void setEditedProductQuantity(String editedProductQuantity) {
+		this.editedProductQuantity = editedProductQuantity;
+	}
+
+	public String getEditedProductMeasureUnit() {
+		return editedProductMeasureUnit;
+	}
+
+	public void setEditedProductMeasureUnit(String editedProductMeasureUnit) {
+		this.editedProductMeasureUnit = editedProductMeasureUnit;
+	}
+
+	public String getEditedProductDescription() {
+		return editedProductDescription;
+	}
+
+	public void setEditedProductDescription(String editedProductDescription) {
+		this.editedProductDescription = editedProductDescription;
 	}
 }
