199b226c324938f527b69025d50eee254a60186e
Radu Toev <radutoev@gmail.com>
2013-03-19 22:03:14 +0200

    minor cleanup

--- price-page-web/src/main/webapp/resources/js/controllers/CategoryCtrl.js
+++ price-page-web/src/main/webapp/resources/js/controllers/CategoryCtrl.js
@@ -8,47 +8,4 @@ angular.module('price-page').controller('CategoryCtrl', function CategoryCtrl($s
        $location.path("/produse/categorie/"+catId.toString());
     }
     $scope.fetch();
-
-    // $scope.fetch = function(){
-    //     $http({
-    //         url : 'rest/categories',
-    //         method : 'GET'
-    //     }).success(function(data, status, headers, configs){
-    //         $scope.categories = data;
-    //     }).error(function(data, status, headers, configs){
-    //         alert('error retrieving categories');
-    //     });
-    // }
-
-    // $scope.fetchStoreTypes = function(){
-    //     $http({
-    //         url : 'rest/store-chain/stores/types',
-    //         method : 'GET'
-    //     }).success(function(data, status, headers, configs){
-    //        $scope.storeTypes = data;
-    //    }).error(function(data, status, headers, configs){
-    //        alert('error retrieving store types');
-    //    });
-    // }
-
-    // $scope.init = function(){
-    //     if($scope.categories === undefined){
-    //         $scope.fetch();
-    //     }
-
-    //     if($scope.storeTypes === undefined){
-    //         $scope.fetchStoreTypes();
-    //     }
-    // }
-
-    // $scope.init();
-
-
-    // $scope.fetchForCateg = function(catId){
-    //    $location.path("/produse/categorie/"+catId.toString());
-    // }
-
-    // $scope.fetchForStoreType = function(storeTypeId){
-    //     $location.path("/produse/tip-magazin/"+storeTypeId.toString());
-    // }
 });
